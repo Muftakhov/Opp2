683b5fc4753329cbc4efa384a213fd9c3e6a1470
мрр <Kappa132@mail.ru>
2023-06-03 15:20:03 +0500

    Opp2

---
+++ src/pro/sky/java/homeworks/course2/lesson1/Bicycle.java
@@ -0,0 +1,11 @@
+package pro.sky.java.homeworks.course2.lesson1;
+
+public class Bicycle extends BicycleTransport {
+    public Bicycle (String modelName){
+        super(modelName,2);
+    }
+
+    public Bicycle (String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+}
---
+++ src/pro/sky/java/homeworks/course2/lesson1/Car.java
@@ -0,0 +1,10 @@
+package pro.sky.java.homeworks.course2.lesson1;
+
+public class Car extends MashinniTransport{
+    public Car (String modelName){
+        super(modelName,4);
+    }
+    public Car(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+}
